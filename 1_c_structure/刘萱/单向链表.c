423696037d212e5010d2db5adf39430d80d7c1ff
liuxuan <1484522055@qq.com>
2017-10-19 13:48:16 +0800

    hhh

--- 1_c_structure/刘萱/单向链表.c
+++ 1_c_structure/刘萱/单向链表.c
@@ -38,3 +38,56 @@ int main (void)
 	printf("\n");
 	return 0;
   }  
+
+
+struct s_list * insert_nodes(struct s_list * head,int n)
+{
+	struct s_list * current=head,*after,*other;
+	while(current->data !=n&&current != NULL){
+		current=current->next;
+	}
+	if(!current) return NULL;
+	after=current->next;
+	other =(struct s_list *)malloc(sizeof(struct s_list));
+	scanf("%d",&other->data);
+	if(after){
+		other->next=after;
+		current->next=other;
+	}
+	else{
+		other->next=NULL;
+		current->next=other;
+	}
+	return current;
+ } 
+ struct s_list * delete_nodes(struct s_list **headp,int n)
+ {
+ 	struct s_list * current=*headp,*prior=*headp;
+ 	while(current->data!=n&&current!=NULL){
+ 		prior=current;
+ 		current=current->next;
+	 }
+	 if (!current){
+	 	return NULL;
+	 }
+	 if (current==*headp){
+	 	*headp=current->next;
+	 }
+	 else{
+	 	prior->next=current->next;
+	 }
+	 free(current);
+	 return current;
+ }
+ 
+ struct s_list * find_nodes_recursive(struct s_list *head,int n)
+ {
+ 	struct s_list * p=head;
+ 	if (p){
+ 		if(p->data==n) return p;
+ 		else {
+ 			find_nodes_recursive(p->next,n);
+		 }
+	 }
+	 else return NULL;
+ }
